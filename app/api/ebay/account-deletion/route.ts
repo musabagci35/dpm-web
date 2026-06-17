d415e700e0148643d4970921712a9c29c4807b9d
driveprimemotor <YOUR_GITHUB_EMAIL>
2026-06-17 15:24:33 -0700

    match ebay verification token

--- app/api/ebay/account-deletion/route.ts
+++ app/api/ebay/account-deletion/route.ts
@@ -5,7 +5,7 @@ export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
 
   const challengeCode = searchParams.get("challenge_code") || "";
-  const verificationToken = "DPM-EBAY-2026-VERIFY";
+  const verificationToken = "driveprimemotors-ebay-verification-2026";
 
   const endpoint =
     "https://www.driveprimemotorsllc.com/api/ebay/account-deletion";
