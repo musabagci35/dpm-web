3c2fa89ae00764e2e8257c3a3da0b2acc3b37928
driveprimemotor <YOUR_GITHUB_EMAIL>
2026-06-24 10:18:30 -0700

    Force dynamic vehicle leads page

--- app/admin/vehicle-leads/page.tsx
+++ app/admin/vehicle-leads/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import Link from "next/link";
 import DeleteVehicleLeadButton from "@/components/admin/DeleteVehicleLeadButton";
 import { proThumb } from "@/lib/cloudinaryImage";
